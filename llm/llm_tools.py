fc754294fd9073769f09bce59c0186e5d0bb7cf2
aldsouza <altondsouza02@gmail.com>
2024-06-22 00:03:32 -0600

    Fix

--- llm/llm_tools.py
+++ llm/llm_tools.py
@@ -1,8 +1,9 @@
 import os
 from typing import Any, Optional
 
+import pymongo
 from langchain_core.pydantic_v1 import BaseModel, Field
-from langchain_core.tools import BaseTool
+from langchain_core.tools import BaseTool, ToolException
 from langchain_core.callbacks import AsyncCallbackManagerForToolRun, CallbackManagerForToolRun
 from pymongo import MongoClient
 
@@ -14,10 +15,12 @@ class GetPatientVitalsWithID(BaseModel):
 
 class GetPatientVitalsWithName(BaseModel):
     user_name: str = Field(description="Patient Name")
+    num_encounters: int = Field(description="Number of Encounters")
 
 
 class GetPatientVitalsWithUserNameTool(BaseTool):
     name:str = "GetPatientVitalsWithUserNameTool"
+    description =
     __client:MongoClient = None
     __USER_COLLECTION = "users"
     __ENCOUNTER_COLLECTION = "encounters"
@@ -31,19 +34,51 @@ class GetPatientVitalsWithUserNameTool(BaseTool):
     def _run(
         self,
         user_name: str,
+        num_encounters:int = 1,
         run_manager: Optional[CallbackManagerForToolRun] = None
     ) -> str:
-        client = self.__client
+        """Fetch User Vitals Synchronously"""
+        client = self.__initVectorDB()
         db = client[os.getenv("MONGODB_NAME")]
         user_collection = db[self.__USER_COLLECTION]
         encounter_collection = db[self.__ENCOUNTER_COLLECTION]
         # First Checking if there are any users with
-        """Fetch User Vitals Synchronously"""
+        user_query = {"name":{
+            "$regex": user_name
+        }}
+        users_count = user_collection.count_documents(user_query)
+        if users_count < 1:
+            return "No user with such name exists as a result"
+        user = user_collection.find_one(user_query)
+        encounter_query = {"user_id":user.get("id")}
+        encounters = encounter_collection.find(encounter_query).sort("date(UTC)",pymongo.DESCENDING)
+        response = ""
+
+        for encounter in encounters:
+            response = response + "____________________________________\n"
+            response = response + "Encounter ID:" +  encounter.get("id") + "\n"
+            response = response + "Date:" + encounter.get("date(UTC)").strftime("%d/%m/%Y, %H:%M:%S") + "\n"
+            response = response + "Vitals:\n"
+            for index, (key, value) in enumerate(encounter.get("vitals").items()):
+                response = response + index + ")" + key + ":" + value +"\n"
+            response = response + "Comments:"+encounter.get("comments")+"\n"
+            response = response + "Prescription:"+ encounter.get("prescription") + "\n"
+        return  response
+
+
+
+
+
+
+
 
     def _arun(
         self,
-        *args: Any,
-        **kwargs: Any,
-    ) -> Any:
+            user_name: str,
+            num_encounters: int = 1,
+            run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Fetch Patient Vitals Asynchronously"""
+        return self._run(user_name,num_encounters,)
 
 
---
+++ llm/test.py
@@ -0,0 +1,16 @@
+import os
+
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_ATLAS_CLUSTER_URI"))
+db = client[os.getenv("MONGODB_NAME")]
+name = "Christine"
+users_count = db["users"].count_documents({"name": {"$regex": name}})
+users = db["users"].find_one({"name": {"$regex": name}})
+encounters = db["encounters"].find({"user_id": users.get("id")}).sort("date(UTC)",pymongo.DESCENDING)
+
+
